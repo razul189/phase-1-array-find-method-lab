d49141801a3126a269019da91c20f45fb189d82e
Ramia Zulfiqar <razulfiqar1@gmail.com>
2023-03-20 19:04:18 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+  function superbowlWin(record) {
+    const result = record.find(function(record) {
+      return record.result === "W";
+    });
+    return result ? result.year : undefined;
+  }
+  
